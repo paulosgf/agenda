b2b6a25a66f1e80d4dbc1a2fef5b6050994234cf
Paulo Ferraz <paulosgf.pf@gmail.com>
2022-11-06 15:19:53 -0300

    - Agenda class adjusts

--- agenda.py
+++ agenda.py
@@ -4,15 +4,15 @@ import contatos
 
 
 class Agenda(contatos.Contatos):
-    dicionario = {}
+    _dicionario = {}
     _contador: int = 1
 
     def __init__(self, nome, telefone, email, aniversario, Id, contador):
 
         super().__init__(nome, telefone, email, aniversario, Id)
         contador = self._contador
-        self.dicionario[contador][nome] = nome
-        self.dicionario[contador][telefone] = telefone
-        self.dicionario[contador][email] = email
-        self.dicionario[contador][aniversario] = aniversario
-        self.dicionario[contador][Id] = Id
+        self._dicionario[contador][nome] = nome
+        self._dicionario[contador][telefone] = telefone
+        self._dicionario[contador][email] = email
+        self._dicionario[contador][aniversario] = aniversario
+        self._dicionario[contador][Id] = Id
