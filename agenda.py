48c28c3c69102caaf72da7552d292eda868255c3
Paulo Ferraz <paulosgf.pf@gmail.com>
2022-11-12 22:29:50 -0300

    - Agenda class upgrade

--- agenda.py
+++ agenda.py
@@ -4,15 +4,33 @@ import contatos
 
 
 class Agenda(contatos.Contatos):
-    _dicionario = {}
     _contador: int = 1
+    _agenda = {}
 
-    def __init__(self, nome, telefone, email, aniversario, Id, contador):
-
+    def __init__(self, nome, telefone, email, aniversario, Id):
         super().__init__(nome, telefone, email, aniversario, Id)
-        contador = self._contador
-        self._dicionario[contador][nome] = nome
-        self._dicionario[contador][telefone] = telefone
-        self._dicionario[contador][email] = email
-        self._dicionario[contador][aniversario] = aniversario
-        self._dicionario[contador][Id] = Id
+        self._agenda[self._contador] = []
+        self._agenda[self._contador].append(nome)
+        self._agenda[self._contador].append(telefone)
+        self._agenda[self._contador].append(email)
+        self._agenda[self._contador].append(aniversario)
+        self._agenda[self._contador].append(Id)
+        self._contador += 1
+
+    def adiciona(self, nome, telefone, email, aniversario, Id):
+        self._agenda[self._contador] = []
+        self._agenda[self._contador].append(super()._set_nome(nome))
+        self._agenda[self._contador].append(super()._set_telefone(telefone))
+        self._agenda[self._contador].append(super()._set_email(email))
+        self._agenda[self._contador].append(super()._set_aniversario(aniversario))
+        self._agenda[self._contador].append(super()._set_id(Id))
+        self._contador += 1
+
+    def remove(self):
+        pass
+
+    def busca(self):
+        pass
+
+    def exibe(self, entrada):
+        return self._agenda[entrada]
