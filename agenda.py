35b84c2fffc402d3351df7f86cbc3290fa15702b
Paulo Ferraz <paulosgf.pf@gmail.com>
2022-11-14 20:55:49 -0300

    - Agenda class - finish main logic

--- agenda.py
+++ agenda.py
@@ -23,21 +23,41 @@ class Agenda:
         self._contato._set_aniversario(aniversario)
         self._contato._set_id(Id)
 
-        if (nome == "") or (telefone == ""):
-            print("Falta nome e/ou telefone!")
-            exit(1)
-
-        self._agenda[self._contador] = self._contato
-        self._contador += 1
-        print("Contato adicionado!")
+        if (
+            (nome == "")
+            or (type(nome) != str)
+            or (telefone == "")
+            or (type(telefone) != str)
+        ):
+            print("Falta nome e/ou telefone, ou formato incorreto!")
+        elif (type(email) != str) or (type(aniversario) != str) or (type(Id) != int):
+            print("Email, aniversario ou Id: Formato incorreto!")
+        else:
+            self._agenda[self._contador] = self._contato
+            self._contador += 1
+            print("Contato adicionado!")
 
     def remove(self, entrada):
         del self._agenda[entrada]
         print("contato excluido!")
 
     def busca(self, padrao):
-        resposta = next(item for item in self._agenda if self._agenda[item]._get_nome() == padrao)
-        return resposta
+        try:
+            resposta = next(
+                item
+                for item in self._agenda
+                if (self._agenda[item]._get_nome() == padrao)
+                or (self._agenda[item]._get_telefone() == padrao)
+            )
+            return [
+                self._agenda[resposta]._get_nome(),
+                self._agenda[resposta]._get_telefone(),
+                self._agenda[resposta]._get_email(),
+                self._agenda[resposta]._get_aniversario(),
+                self._agenda[resposta]._get_id(),
+            ]
+        except StopIteration:
+            print("Nome nao encontrado!")
 
     def exibe(self, entrada):
         return [
